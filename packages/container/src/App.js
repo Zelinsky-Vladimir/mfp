40cc543d50bb3bdf55ff4140dc0a1bd0a9d33bc3
Volodymyr Zelinskyi <volodymyr.zelinskyi@pm.bet>
2021-01-10 19:56:20 +0200

    fixed cont

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'ma',
+  productionPrefix: 'con',
 });
 
 const App = () => {
