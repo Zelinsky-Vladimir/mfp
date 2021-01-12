70326a86973a1acba53ab431f0923e23c2524a4a
Volodymyr Zelinskyi <volodymyr.zelinskyi@pm.bet>
2021-01-12 23:26:09 +0200

    removed entry div

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -11,20 +11,18 @@ const generateClassName = createGenerateClassName({
 
 const App = ({ history, onSignIn }) => {
   return (
-    <div>
-      <StylesProvider generateClassName={generateClassName}>
-        <Router history={history}>
-          <Switch>
-            <Route path="/auth/signin">
-              <Signin onSignIn={onSignIn} />
-            </Route>
-            <Route path="/auth/signup">
-              <Signup onSignIn={onSignIn} />
-            </Route>
-          </Switch>
-        </Router>
-      </StylesProvider>
-    </div>
+    <StylesProvider generateClassName={generateClassName}>
+      <Router history={history}>
+        <Switch>
+          <Route path="/auth/signin">
+            <Signin onSignIn={onSignIn} />
+          </Route>
+          <Route path="/auth/signup">
+            <Signup onSignIn={onSignIn} />
+          </Route>
+        </Switch>
+      </Router>
+    </StylesProvider>
   );
 };
 
